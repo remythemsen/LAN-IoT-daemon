31409e285713444505a0bd047df017b6988512fd
remeeh <remeeh@gmail.com>
2017-10-28 23:03:23 +0200

    cleaning code

--- liotpm.py
+++ liotpm.py
@@ -10,25 +10,31 @@ import json
 from lifxlan import LifxLAN
 from lifxlan import Light
 
-class TPPlugBrigde():
+class TPPlugBridge():
 	def apply(self, settings):
+		pass
+	
+	def state(self, device_ref):
 		pass
 
 
-
 class LifxBridge():
-	def	apply(self, settings):
-		for device_profile in jObject:
-			try:
-				duration = device_profile['duration']												#milliseconds
-				dev = Light(device_profile['mac'],device_profile['ip'])
-				dev.set_power(device_profile['power'], duration) 						#range [0,65535]
-				dev.set_hue(device_profile['hue'], duration) 								#range [0-65535]
-				dev.set_saturation(device_profile['saturation'], duration)	#range [0-65535]
-				dev.set_brightness(device_profile['brightness'], duration) 	#range [0-65535]
-				dev.set_colortemp(device_profile['temperature'], duration) 	#range [2500-9000]
-			except:
-				continue
+	def	apply(self, device_profile):
+		duration = device_profile['duration']												#milliseconds
+		dev = Light(device_profile['mac'],device_profile['ip'])
+		dev.set_power(device_profile['power'], duration) 						#range [0,65535]
+		dev.set_hue(device_profile['hue'], duration) 								#range [0-65535]
+		dev.set_saturation(device_profile['saturation'], duration)	#range [0-65535]
+		dev.set_brightness(device_profile['brightness'], duration) 	#range [0-65535]
+		dev.set_colortemp(device_profile['temperature'], duration) 	#range [2500-9000]
+	
+	def state(self, device):
+		bulbRef = Light(device['mac'],device['ip'])
+		hsbk = bulbRef.get_color()
+		dev = Device(device['mac'],device['ip'],bulbRef.get_power(),hsbk[0],hsbk[1],hsbk[2],hsbk[3])
+
+		return json.dumps(dev.__dict__)
+
 		
 
 class Device(object):
@@ -43,63 +49,65 @@ class Device(object):
 		
 def selectBridge(x):
 	switcher = {
-		"bulb": LifxBridge()
+		"bulb": LifxBridge(),
+		"plug": TPPlugBridge()
 	}
-	return switcher.get(x, "nothing")
+	return switcher.get(x.lower(), "Unknown bridge was provided.")
 
 
 def help_message():
 	return "parameters are of the form:\nprogram.py state|apply [json formatted profile]"
-# Takes 'profile' in json stdin
 
-#stdin
-operation = sys.argv[1]
-if(operation == "state"):
-	print("Getting state")
+def get_states(json_formatted_devices):
 	#Read state from each device in jsonString, return json formatted string
+	result = '['
+	if not json_formatted_devices.strip():
+		raise Exception("You need to provide a JSON formatted device list, empty string was found.")
 	
-	# Parse 
-	if len(sys.argv) < 3:
-		# show all discoverable lights
-		# TODO 
-		print("passing...")
-		pass
-	else: 
-		jsonString = sys.argv[2]
-		jObject = json.loads(jsonString)
-
-		devices_in_JSON = []
-		for device in jObject['devices']:
+	device_list = json.loads(json_formatted_devices)
+	if(len(device_list) == 0):
+		return result + ']'	
+	else:
+		for device in device_list[:-1]:
+			bridge = selectBridge(device['device_type']) 
+			# Action
 			try:
-				bulbRef = Light(device['mac'],device['ip'])
-				hsbk = bulbRef.get_color()
-				dev = Device(device['mac'],device['ip'],bulbRef.get_power(),hsbk[0],hsbk[1],hsbk[2],hsbk[3])
-				devices_in_JSON.append(json.dumps(dev.__dict__))
+				result += bridge.state(device) + ','
 			except:
-				continue
+				continue #TODO log this occurrence
 
-		result = ""
-		if(len(devices_in_JSON) > 0):
-			result = '{"devices":[' + devices_in_JSON[0] 
+		try:
+			last_device = device_list[len(device_list-1)]
+			bridge = selectBridge(last_device['device_type'])
+			result += bridge.state(last_device)
+		except:
+			pass #TODO log this occurrence
 
-			for i in range(1, len(devices_in_JSON)):
-				result += ',' + devices_in_JSON[i] 
+		return result + ']'	
 
-			result += ']}'
-		else:
-			result = '{"devices":[]}'
-
-		print(result)
+def apply_settings(json_formatted_device_settings):
 	
-elif(operation == "apply"):
-	# Parse 
-	for line in sys.stdin:
-		jObject = json.loads(line)
-		for setting in jObject:
+	if not json_formatted_device_settings.strip():
+		raise Exception("You need to provide a JSON formatted device list, empty string was found.")
+	
+	device_list = json.loads(json_formatted_device_settings)
+	if(len(device_list) == 0):
+		raise Exception("Empty device list was provided")	
+	else:
+		for setting in device_list:
 			bridge = selectBridge(setting['device_type']) 
 			# Action
-			bridge.apply(setting)
+			try:
+				bridge.apply(setting)
+			except:
+				continue #TODO log this occurence
+	
 
+operation = sys.argv[1]
+if(operation.lower() == "state"):
+	print(get_states(sys.stdin.read()))
+elif(operation == "apply"):
+	apply_settings(sys.stdin.read())
 elif(operation == "help"):
 		print(help_message())
 else:
